b1b15ffa43d7d1c9d71417eea9d94ff06b93b570
Diego Neri <diego@diegoneri.com>
2024-11-29 20:34:55 -0300

    feat(): lista de produtos

---
+++ Produto.cs
@@ -0,0 +1,6 @@
+public class Produto
+{
+    public int Codigo { get ; set; }
+    public string Descricao { get ; set; } = default!;
+    public decimal Preco { get ; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("-----------------------------");
+Console.WriteLine("---- Carrinho de Compras ----");
+Console.WriteLine("-----------------------------");
+
+var produtosDisponiveis = new List<Produto>()
+{
+    new Produto() { Codigo = 1, Descricao = "Camisa P", Preco = 40},
+    new Produto() { Codigo = 2, Descricao = "Camisa M", Preco = 45},
+    new Produto() { Codigo = 3, Descricao = "Camisa G", Preco = 50},
+    new Produto() { Codigo = 4, Descricao = "Camisa GG", Preco = 55}
+};
+
+ExibirListaDeProdutos(produtosDisponiveis);
+
+void ExibirListaDeProdutos(List<Produto> produtos)
+{
+    Console.WriteLine($"| Código | Descrição   | Valor unitário |");
+    Console.WriteLine($"| ------ | ----------- | -------------- |");
+
+    produtos.ForEach(produto => {
+        Console.WriteLine($"| {produto.Codigo}      | {produto.Descricao}    | {produto.Preco:C2}       |");    
+    });
+
+    // for (int i = 0; i < produtosDisponiveis.Count; i++)
+    // {
+    //     Produto produto = produtosDisponiveis[i];
+    //     Console.WriteLine($"| {produto.Codigo}   | {produto.Descricao}  | {produto.Preco:C2} |");    
+    // }
+
+    // foreach(var produto in produtos)
+    // {
+    //     Console.WriteLine($"| {produto.Codigo}   | {produto.Descricao}  | {produto.Preco:C2} |");    
+    // } 
+}
